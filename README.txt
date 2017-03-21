88a1897ee4fb4e60586931821b41ae633d1dd20b
hemlo <hemlocked@outlook.com>
2017-03-21 15:30:54 -0500

    Moving CattleTrax

---
+++ README.txt
@@ -0,0 +1,63 @@
+Installation Notes
+
+OS:  linux (ubuntu server)
+Apache2 web server
+MySql
+PHP
+
+https://www.digitalocean.com/community/tutorials/how-to-install-linux-apache-mysql-php-lamp-stack-on-ubuntu-16-04
+
+Python:
+Sqlalchemy:
+sudo apt-get install mysql-server
+sudo apt-get install mysql-client
+sudo apt-get install libmysqlclient15-dev
+Second step,install the python-mysqldb:
+
+sudo apt-get install python-mysqldb
+//Third step,install the easy_install:
+
+sudo wget http://peak.telecommunity.com/dist/ez_setup.py
+sudo python ez_setup.py
+//Forth step,install the MySQL-Python:
+
+sudo easy_install MySQL-Python
+//Finally,sqlalchemy:
+
+sudo easy_install SQLAlchemy
+
+PySerial:
+download tarball from https://pypi.python.org/pypi/pyserial
+untar and run:
+sudo python setup.py install
+
+TODO:
+
+Database:
+Add/Delete Cow Procedure
+Update Cow Procedure
+Add vaccination data.  Need to figure out how to actually store/display these.  
+Add Users/Logins
+Expand search functionality to include herd and pasture.
+
+
+RPI:
+Learn more about SQL Alchemy's timeouts, connection pooling, etc., to improve functionality
+Format log file.  Should we create our own error messages/codes?
+Pi will lose data if it has lost signal, read tags, then lost power before reconnecting to the DB.
+	How can we correct this?
+
+
+HTML:
+Make list of animals that visited feeder clickable.  Click calls pop-up that displays vitals.
+Validate input for all of our user fields.
+Create user login and authentications
+
+
+24 Hour Maintenance:  
+Daily report including upcoming vaccination dates for animals, upcoming delivery dates, animals 
+	that have not visited the mineral feeder in 3 days.  
+
+
+
+
---
+++ pfunctions.php
@@ -0,0 +1,86 @@
+<?php
+	session_start();
+
+	$type = $_POST['type'];
+	if($type == '24'){
+		getData(1);
+	}else if($type == '7'){
+		getData(7);
+	}else if ($type == '30'){
+		getData(30);
+	}else if($type == 'db_search'){
+		search();
+	}
+	//print($type);
+	//foo();
+	//get24();
+
+	function login(){
+		//$host="149.149.150.136";
+		//$username = "pjtinker";
+		$host="localhost";
+		$username = "root";
+		$password = "CattleTrax11!";
+		$db_name = "cattletrax";
+
+		try{
+			$db = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $db;
+		}catch(PDOException $e){
+			print($e->getMessage());
+		}
+	}
+	function getData($value){
+		$db = login();
+
+		try{
+			$stmt = $db->prepare('SELECT * FROM feeder where DATEDIFF(NOW(), last_visit_date) between 0 and :value');
+			$stmt->bindParam(':value', $value, PDO::PARAM_INT);
+			$stmt->execute();
+			//$header = $stmt->fetch(PDO::FETCH_ASSOC);
+/*			Don't believe I need this if we're parsing in jquery
+			foreach ($header as $field => $value){
+				echo json_encode($field);
+			}*/
+			//$stmt = $db->query('SELECT * FROM feeder where DATEDIFF(NOW(), last_visit_date) between 0 and 30');
+			$rows = array();
+			while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+				$rows[] = $row;
+				//echo json_encode(echo $row['ref_id'] ." ". $row['num_visits'] . $row['last_visit_date']."\n");
+				
+			}
+			print json_encode($rows);
+		}catch(PDOException $e){
+			$arr = array('error' => $e->getMessage());
+			print json_encode($arr);
+
+		}
+		
+	}
+	function search(){
+		$db = login();
+		try{
+			//$stmt = $db->prepare('SELECT * FROM vitals WHERE animal_id = (SELECT animal_id FROM animal where tag_id LIKE :tag)');
+			$stmt = $db->prepare('SELECT vit.* FROM vitals as vit INNER JOIN animal as ani on vit.animal_id = ani.animal_id WHERE ani.tag_id LIKE :tag');
+			if($_POST['tag_id'] > 0){
+				$tag = "%".$_POST['tag_id']."%";
+			}else{
+				$tag = "%".$_POST['animal_id']."%";
+			}
+			$stmt->bindParam(':tag', $tag);
+			$stmt->execute();
+			$rows = array();
+			while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+				$rows[] = $row;
+			}
+			print json_encode($rows);
+		}catch(PDOException $e){
+			$arr = array('error' => $e->getMessage());
+			print json_encode($arr);
+		}
+
+	}
+
+?>
+
